6b11bed46427cea4a595d5d2cdcbeffb8bddd2bc
unknown <sulaiman.kadyrov@gmail.com>
2023-07-24 13:42:40 +0600

    dz5

--- js/home_works.js
+++ js/home_works.js
@@ -145,3 +145,99 @@ startButton.addEventListener('click', startCounter);
 stopButton.addEventListener('click', stopCounter);
 resumeButton.addEventListener('click', resumeCounter);
 clearButton.addEventListener('click', clearCounter);
+
+
+/////////////////////////weather//////////////////////////////
+
+const apiKey = '863242cfb2b1d357e6093d9a4df19a4b'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+
+    if(response.status === 404){
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    }else {
+        const data = await response.json()
+
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+
+        if (data.weather[0].main === 'Clouds') {
+            weatherIcon.src = '../images/clouds.png'
+        } else if (data.weather[0].main === 'Clear') {
+            weatherIcon.src = '../images/clear.png'
+        } else if (data.weather[0].main === 'Rain') {
+            weatherIcon.src = '../images/rain.png'
+        } else if (data.weather[0].main === 'Drizzle') {
+            weatherIcon.src = '../images/drizzle.png'
+        } else if (data.weather[0].main === 'Mist') {
+            weatherIcon.src = '../images/mist.png'
+        }
+
+        document.querySelector('.weather').style.display = 'block'
+        document.querySelector('.error').style.display = 'none'
+
+    }
+}
+
+searchBox.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault(); // Предотвращает отправку формы при нажатии Enter
+        checkWeather(searchBox.value);
+    }
+});
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value)
+})
+
+
+///////////// converter ////////////
+
+const som = document.querySelector('#som');
+const usd = document.querySelector('#usd');
+const eur = document.querySelector('#eur');
+
+
+const convert = (inputCurrency, targetCurrency1, targetCurrency2) => {
+    inputCurrency.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../currency.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+
+        request.onload = () => {
+            const response = JSON.parse(request.response);
+            if (inputCurrency === som) {
+                targetCurrency1.value = (inputCurrency.value / response.usd).toFixed(2);
+                targetCurrency2.value = (inputCurrency.value / response.eur).toFixed(2);
+            } else if (inputCurrency === usd) {
+                targetCurrency1.value = (inputCurrency.value * response.usd).toFixed(2);
+                targetCurrency2.value = (inputCurrency.value * response.usd / response.eur).toFixed(2);
+            } else if (inputCurrency === eur) {
+                targetCurrency1.value = (inputCurrency.value * response.eur).toFixed(2);
+                targetCurrency2.value = (inputCurrency.value * response.eur / response.usd).toFixed(2);
+            }
+            inputCurrency.value === '' && (targetCurrency1.value = targetCurrency2.value = '');
+        };
+    };
+};
+
+convert(som, usd, eur);
+convert(usd, som, eur);
+convert(eur, som, usd);
+
+const clearConverter = document.querySelector('#reset-converter')
+clearConverter.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input) => {
+        input.value = '';
+    })
+})
